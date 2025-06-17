1da88f3955dfb90df2c34e12d41e9d7fae1c170b
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2025-06-17 20:33:40 -0300

    Se creo la carpeta formulario socio

---
+++ frontend/src/app/components/forms/form-socio/form-socio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-socio',
+  imports: [],
+  templateUrl: './form-socio.component.html',
+  styleUrl: './form-socio.component.css'
+})
+export class FormSocioComponent {
+
+}
