2c66d0441c8ee61ac998e01755c58a45f74e388c
SaraMercado10 <sara.denise.mercado.12@gmail.com>
2025-06-17 13:55:47 -0300

    Subo front de login
    Subo model de usuario, servicio para el login.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component'; //Después borrar esto
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent], //Despues borrar el import de login
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
