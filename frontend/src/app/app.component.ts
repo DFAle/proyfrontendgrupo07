a01f5eda74a74b520d5023ebf0e733e22270e27e
aAleSJ <thealecuellar@gmail.com>
2025-06-17 20:17:31 -0300

    agregando el header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/auth/login/login.component'; //Después borrar esto
+import { LoginComponent } from './components/auth/login/login.component';
+import { HeaderComponent } from "./components/layout/header/header.component"; //Después borrar esto
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent], //Despues borrar el import de login
+  imports: [RouterOutlet, LoginComponent, HeaderComponent], //Despues borrar el import de login
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
