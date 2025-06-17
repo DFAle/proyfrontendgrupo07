add1b59f445607d364216f30a6d2c5c6dac63c3f
aAleSJ <thealecuellar@gmail.com>
2025-06-17 20:54:05 -0300

    rutas

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/auth/login/login.component';
 import { HeaderComponent } from "./components/layout/header/header.component"; //Después borrar esto
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, HeaderComponent], //Despues borrar el import de login
+  imports: [RouterOutlet,HeaderComponent], //Despues borrar el import de login
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
