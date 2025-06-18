1d157c2a2aba7e32c2c7666c189e47f012750c96
x6Nahuel <gaspargonzalo024@gmail.com>
2025-06-18 00:38:43 -0300

    rutas de los link

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { FormSocioComponent } from './components/forms/form-socio/form-socio.component';
+import { FormAsistenciaComponent } from './components/forms/form-asistencia/form-asistencia.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
-    {path:'register',component:FormSocioComponent}
+    {path:'register',component:FormSocioComponent},
+    {path: 'registrarAsistencia',component:FormAsistenciaComponent}
 ];
