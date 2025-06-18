105173132b27a4d657d5781afbd76f8a23c9acb7
DFAle <aale0800z@gmail.com>
2025-06-18 01:18:07 -0300

    ale

---
+++ frontend/src/app/components/Pages/personal-mesa/personal-mesa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-mesa',
+  imports: [],
+  templateUrl: './personal-mesa.component.html',
+  styleUrl: './personal-mesa.component.css'
+})
+export class PersonalMesaComponent {
+
+}
--- frontend/src/app/components/forms/form-asistencia/form-asistencia.component.ts
+++ frontend/src/app/components/forms/form-asistencia/form-asistencia.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-asistencia',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './form-asistencia.component.html',
   styleUrl: './form-asistencia.component.css'
 })
