c9dfd32356ba683dd20d85330641d8cec935f46a
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2025-06-18 18:53:19 -0300

    Creacion del home y modificacion de rutas
    Se creo el componente header-home junto con la configuracion de la navegacion.
    
    Se creo El COMPONENTE INDEX que se encuentra dentro de la carpeta home
    el cual actua como un contendedor de las paginas del home como las
    actividades, profesores, etc.
    
    Se creo el componente principal, profesor y actividad.
    
    Se configuro las rutas del home y tambien se modifico el APP.COMPONENT
    para ocultar los header dependiendo de la ruta al cual se accede.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/layout/header/header.component";
 import { FooterComponent } from "./components/layout/footer/footer.component"; //Después borrar esto
+import { CommonModule } from '@angular/common';
+import { HeaderHomeComponent } from "./components/layout/header-home/header-home.component";
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent], //Despues borrar el import de login
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent, 
+    HeaderHomeComponent], //Despues borrar el import de login
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  mostrarHeader$: Observable<boolean>;
+  mostrarHeaderHome$: Observable<boolean>;
+
+  constructor(private router: Router) {
+
+       this.mostrarHeader$ = this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => {
+       
+        return event.urlAfterRedirects === '/';
+      })
+    );
+    
+     this.mostrarHeaderHome$ = this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => {
+        // Contiene las rutas de la pagina home
+        const homeRoutes = ['/home', '/home/actividad', '/home/profesor'];
+        return homeRoutes.includes(event.urlAfterRedirects);
+      })
+    );
+  }
+  
+  ngOnInit(): void {
+  }
+
+
+ 
+}
+
+export class App {
   title = 'frontend';
 }
