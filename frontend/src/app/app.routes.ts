1cfefdf3baba6478f164d354d6482d5254ffdae1
DFAle <aale0800z@gmail.com>
2025-06-18 00:56:21 -0300

    ale

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { FormSocioComponent } from './components/forms/form-socio/form-socio.component';
+import { PersonalMesaComponent } from './components/Pages/personal-mesa/personal-mesa.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
-    {path:'register',component:FormSocioComponent}
+    {path:'register',component:FormSocioComponent},
+    {path:'personal-mesa',component:PersonalMesaComponent}
 ];
---
+++ frontend/src/app/components/Pages/personal-mesa/personal-mesa.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-personal-mesa',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './personal-mesa.component.html',
+  styleUrl: './personal-mesa.component.css'
+})
+export class PersonalMesaComponent {
+
+}
