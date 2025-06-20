f0e8b7c57d459ae9838a76813dc5df00fe0af97c
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2025-06-20 18:30:54 -0300

    Modificacion de ruta, home
    Se modifico la ruta para que ejecute el home por defecto cada vez que se
    inicializala aplicacion.
    Se modifico el footer para agregar el mapa de ubicacion del complejo o
    del club.
    Se agrego el modelo y service de noticia para consumir una api de
    deporte para la parte del home.
    Se modifico el home para que consuma la api de noticia.

--- frontend/src/app/components/Pages/home/page/principal/principal.component.ts
+++ frontend/src/app/components/Pages/home/page/principal/principal.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from '../../../../../services/noticia.service';
+import { Noticia } from '../../../../../models/noticia';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-principal',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+  
+  noticias!:Array<Noticia>;
+  noticia!:Noticia;
+  
+  constructor(private noticiaService:NoticiaService){
+    this.cargarNoticias();
+  }
+
+cargarNoticias(){
+this.noticiaService.getNoticias().subscribe(
+   (result) => {
+    this.noticias = [];
+   result.data.forEach( (element: any) => {
+     this.noticia = new Noticia(element.title,element.description,element.thumbnail)
+     this.noticias.push(this.noticia)
+   });
+   },
+  (error) => {
+        console.error("Error en la petición:", error);
+      })
+}
+
 
 }
