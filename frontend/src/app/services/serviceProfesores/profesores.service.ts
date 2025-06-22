bb893ebfc4a7b44bc8d441839d6ca4ad3090cee4
DFAle <aale0800z@gmail.com>
2025-06-22 17:28:09 -0300

    profes

---
+++ frontend/src/app/services/serviceProfesores/profesores.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfesoresService {
+
+  urlBase :string = "http://localhost:3000/api/profesor/"
+  constructor(private _http:HttpClient) { 
+    
+  }
+
+  public getProfesores():Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        
+      }),
+      params : new HttpParams()
+    }
+    return this._http.get(this.urlBase, httpOptions);
+  }
+}
