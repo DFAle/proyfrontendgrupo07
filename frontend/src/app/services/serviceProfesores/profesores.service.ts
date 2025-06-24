c67056cde396491bfffaf48a374e7073232ba229
x6Nahuel <gaspargonzalo024gmail.com>
2025-06-24 17:39:17 -0300

    add profesor

--- frontend/src/app/services/serviceProfesores/profesores.service.ts
+++ frontend/src/app/services/serviceProfesores/profesores.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Profesores } from '../../models/Profesores/profesores';
 
 @Injectable({
   providedIn: 'root'
@@ -21,4 +22,14 @@ export class ProfesoresService {
     }
     return this._http.get(this.urlBase, httpOptions);
   }
+
+   addProfesor(profesor: Profesores): Observable<any> {
+      let httpOpttion = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+      };
+      let body: any = JSON.stringify(profesor);
+      return this._http.post(this.urlBase, body, httpOpttion);
+    }
 }
