39999da07c06a13e9d7a9deccda912e885adfbd7
x6Nahuel <gaspargonzalo024gmail.com>
2025-07-01 04:49:50 -0300

    add home and form asistencia

--- frontend/src/app/components/Pages/home/page/principal/principal.component.ts
+++ frontend/src/app/components/Pages/home/page/principal/principal.component.ts
@@ -12,46 +12,59 @@ import { GeminisService } from '../../../../../services/serviceGoogle/serviceGem
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
-  
-  noticias!:Array<Noticia>;
-  noticia!:Noticia;
-  
-  constructor(private noticiaService:NoticiaService, private actividadService:ActividadService,
-    private geminisService:GeminisService){
-   // this.cargarNoticias();
+
+  noticias!: Array<Noticia>;
+  noticia!: Noticia;
+
+  constructor(private noticiaService: NoticiaService, private actividadService: ActividadService,
+    private geminisService: GeminisService) {
+    this.cargarNoticias();
+    this.cargarNoticiaPropia();
   }
 
-/* 
-cargarNoticias(){
-this.noticiaService.getNoticias().subscribe(
-   (result) => {
-    this.noticias = [];
-   result.data.forEach( (element: any) => {
-     this.noticia = new Noticia(element.title,element.description,element.thumbnail)
-     this.noticias.push(this.noticia)
-   });
-   },
-  (error) => {
+
+  cargarNoticias() {
+    this.noticiaService.getNoticias().subscribe(
+      (result) => {
+        this.noticias = [];
+        result.data.forEach((element: any) => {
+          this.noticia = new Noticia(element.title, element.description, element.thumbnail)
+          this.noticias.push(this.noticia)
+        });
+      },
+      (error) => {
         console.error("Error en la petición:", error);
       })
-}*/
+  }
+  nuestraNoticia:Array<any>=[];
+    cargarNoticiaPropia() {
+    this.actividadService.consumirActividad().subscribe(
+      (result) => {
+       console.log(result);
+       this.nuestraNoticia=result;
+       
+      },
+      (error) => {
+        console.error("Error en la petición:", error);
+      })
+  }
 
-  
-/*
-generarTexto(prompt: string): Observable<string> { // Ahora retorna un Observable<string>
-  return this.geminisService.generarTexto(prompt).pipe(
-    map((result: any) => {
-      const generatedText = result.candidates[0].content.parts[0].text;
-      console.log("Texto generado por IA:", generatedText); // Para depuración
-      return generatedText;
-    }),
-    catchError((error) => {
-      console.error("Error al generar texto con Gemini:", error);
-      return of(''); // Retorna un Observable vacío o un string por defecto en caso de error
-    })
-  );
-}
-*/
+
+  /*
+  generarTexto(prompt: string): Observable<string> { // Ahora retorna un Observable<string>
+    return this.geminisService.generarTexto(prompt).pipe(
+      map((result: any) => {
+        const generatedText = result.candidates[0].content.parts[0].text;
+        console.log("Texto generado por IA:", generatedText); // Para depuración
+        return generatedText;
+      }),
+      catchError((error) => {
+        console.error("Error al generar texto con Gemini:", error);
+        return of(''); // Retorna un Observable vacío o un string por defecto en caso de error
+      })
+    );
+  }
+  */
 
 
 
